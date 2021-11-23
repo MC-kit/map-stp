d917cd5cb6b3bd57ec8089e77b5af1632b17071f
dvp <dmitri_portnov@yahoo.com>
2021-11-23 22:15:44 +0300

    fix: relax pattern to search meta info in STP component names
    On 'unsharing' of bodies in SpaceClaim it adds additional ids to
    the end of name. So, our labels will be not at end of name.
    We have to relax pattern to search just to square brackets.

--- tests/test_extract_info.py
+++ tests/test_extract_info.py
@@ -12,24 +12,38 @@ def test_load_materials_index(materials):
 
 
 @pytest.mark.parametrize(
-    "paths, expected",
+    "paths, expected, msg",
     [
-        ([["aaa [m-LH]", "bbb", "ccc0"]], [(2, 0.14822, None, None)]),
-        ([["aaa [m-LH]", "bbb[f-0.9]", "ccc0"]], [(2, 0.14822, 0.9, None)]),
+        (
+            [["aaa [m-LH]", "bbb", "ccc0"]],
+            [(2, 0.14822, None, None)],
+            "Just material (LiH)",
+        ),
+        (
+            [["aaa [m-LH]", "bbb[f-0.9]", "ccc0"]],
+            [(2, 0.14822, 0.9, None)],
+            "Material and factor",
+        ),
         (
             [["aaa [m-LH]", "bbb[f-0.99]", "ccc0[r-PBS55]"]],
             [(2, 0.14822, 0.99, "PBS55")],
+            "Material, factor, and RWCL id",
+        ),
+        (
+            [["aaa [m-LH].1", "bbb[f-0.99]", "ccc0[r-PBS55]"]],
+            [(2, 0.14822, 0.99, "PBS55")],
+            "Should recognize material label, which is not at the end of name.",
         ),
     ],
 )
-def test_extract_info(materials, paths, expected):
+def test_extract_info(materials, paths, expected, msg):
     _expected = pd.DataFrame.from_records(
         expected, columns="number density factor rwcl".split()
     )
     actual = extract_path_info(paths, materials)
     isnull = actual.isnull()
-    assert (isnull == _expected.isnull()).all(axis=None)
-    assert (actual == _expected).mask(isnull).all(axis=None)
+    assert (isnull == _expected.isnull()).all(axis=None), msg
+    assert (actual == _expected).mask(isnull).all(axis=None), msg
 
 
 if __name__ == "__main__":
