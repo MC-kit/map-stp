184089e4838794d404c7843e0a1a2bf8c5647cd5
dvp <dmitri_portnov@yahoo.com>
2021-11-30 05:23:17 -0800

    build: conda reset environment script is fixed

---
+++ dev/install-to-windows-env.bat
@@ -0,0 +1,138 @@
+@echo off
+::
+:: Prepare conda environment for mapstp development on Windows.
+::
+:: dvp, Nov 2021
+::
+
+if "%1"=="--help" (
+    echo.
+    echo Usage:
+    echo.
+    echo "reset-conda-env <conda_env> <python_version>"
+    echo.
+    echo All the parameters are optional.
+    echo.
+    echo Defaults:
+    echo 	conda_env=mapstp-2
+::    echo   install_tool=pip   another valid value: poetry
+    echo   python_version=3.9
+    echo.
+    goto END
+)
+
+set conda_env=%1
+shift
+if "%conda_env%"=="" set conda_env=mapstp-2
+echo  Installing environment %conda_env%
+
+set python_vesion=%1
+shift
+if "%python_vesion%"=="" set python_vesion=3.9
+echo  Python version %python_version%
+
+call poetry --version > NUL
+if errorlevel 1 (
+	echo ERROR\: Poetry is not available
+	echo Run dev\install-poetry.bat
+	echo or see poetry installation instructions\: https\://python-poetry.org
+	goto END
+
+)
+
+echo Installing conda environment %conda_env%
+
+call conda deactivate
+call conda activate
+call conda env remove -n %conda_env% -q -y
+call conda create -n %conda_env% python=%python_version% -q -y
+call conda activate %conda_env%
+python --version
+
+
+::   this makes poetry to use conda environment and don't create own one
+call poetry config --local virtualenvs.create false
+::   this creates egg-link in the environment to current project (development install)
+echo Poetry path\:
+poetry env info --path
+echo Conda environment path\:
+
+
+
+:: cannot install directly with poetry: some errors on accessing packages, so use pip to preinstall dependencies
+:: cannot use "development" install (-e) with pip
+pip install .
+poetry export --format=requirements.txt --dev --without-hashes >| requirements-dev.txt
+pip install --requirement requirements-dev.txt
+del requirements-dev.txt
+
+
+:: make it "development" install
+call poetry install
+
+if errorlevel 1  (
+    echo "ERROR: failed to run install with %install_tool%"
+    goto END
+)
+
+mapstp --version
+if errorlevel 1  (
+    echo "ERROR: failed to install mapstp"
+    goto END
+)
+echo.
+echo SUCCESS: mapstp has been installed into conda environment %conda_env%
+echo.
+
+
+nox
+if errorlevel 1 (
+    echo ERROR: failed to run tests
+    goto END
+)
+echo.
+echo SUCCESS: nox test is passed OK
+echo.
+
+
+
+if "%install_tool%"=="poetry" (
+    :: verify nox
+    nox --list
+    :: safety first - run this on every dependency addition or update
+    :: test often - who doesn't?
+    nox -s safety -s tests -p %python_version% -- -m "not slow" --cov
+    call poetry build
+    if errorlevel 1 (
+        echo ERROR: failed to run poetry build
+        goto END
+    )
+) else (
+    pip install .
+    if errorlevel 1 (
+        echo ERROR: failed to collect dependencies with pip
+        goto END
+    )
+)
+
+call create-jk %mapstp%
+if errorlevel 1 (
+    goto END
+)
+
+echo.
+echo SUCCESS!
+echo --------
+echo.
+echo Usage:
+echo.
+mapstp --help
+echo.
+echo Conda environment %mapstp% is all clear.
+echo Set your IDE to use %CONDA_PREFIX%\python.exe
+echo.
+echo Enjoy!
+echo.
+
+
+:END
--- dev/reset-conda-env.bat
+++ dev/reset-conda-env.bat
@@ -5,7 +5,27 @@
 :: dvp, Nov 2021
 ::
 
-if "%1"=="--help" (
+:: defaults
+set conda_env=mapstp
+set python_version=3.10
+
+
+call :main %*
+
+if ""%ERRORLEVEL%"" NEQ "0" (echo SUCCESS.) else (echo FAIL!)
+
+goto END
+
+:main
+    if "%1"=="--help" (
+        call :usage
+    ) else (
+        call :arg_parse %*
+        call :reset_conda
+    )
+    goto END
+
+:usage
     echo.
     echo Usage:
     echo.
@@ -14,116 +34,30 @@ if "%1"=="--help" (
     echo All the parameters are optional.
     echo.
     echo Defaults:
-    echo 	conda_env=mapstp-2
-::    echo   install_tool=pip   another valid value: poetry
-    echo   python_version=3.9
+    echo    conda_env=%conda_env%
+    echo    python_version=%python_version%
     echo.
-    goto END
-)
 
-set conda_env=%1
-shift
-if "%conda_env%"=="" set conda_env=mapstp-2
-echo  Installing environment %conda_env%
-
-set python_vesion=%1
-shift
-if "%python_vesion%"=="" set python_vesion=3.9
-echo  Python version %python_version%
-
-call poetry --version > NUL
-if errorlevel 1 (
-	echo ERROR\: Poetry is not available
-	echo Run dev\install-poetry.bat
-	echo or see poetry installation instructions: https://python-poetry.org
-	goto END
-
-)
-
-echo Installing conda environment %conda_env%
-
-call conda deactivate
-call conda activate
-call conda env remove -n %conda_env% -q -y
-call conda create -n %conda_env% python=%python_version% -q -y
-call conda activate %conda_env%
-python --version
-
-
-::   this makes poetry to use conda environment and don't create own one
-call poetry config --local virtualenvs.create false
-::   this creates egg-link in the environment to current project (development install)
-poetry env info --path
-
-
-
-::goto END
-
-call poetry install
-
-if errorlevel 1  (
-    echo "ERROR: failed to run install with %install_tool%"
-    goto END
-)
-
-mapstp --version
-if errorlevel 1  (
-    echo "ERROR: failed to install mapstp"
-    goto END
-)
-echo.
-echo SUCCESS: mapstp has been installed into conda environment %conda_env%
-echo.
-
-
-pytest -m "not slow"
-if errorlevel 1 (
-    echo ERROR: failed to run tests
-    goto END
-)
-echo.
-echo SUCCESS: pytest is passed OK
-echo.
-
-
-
-if "%install_tool%"=="poetry" (
-    :: verify nox
-    nox --list
-    :: safety first - run this on every dependency addition or update
-    :: test often - who doesn't?
-    nox -s safety -s tests -p %python_version% -- -m "not slow" --cov
-    call poetry build
-    if errorlevel 1 (
-        echo ERROR: failed to run poetry build
-        goto END
+:arg_parse
+    if "%1" NEQ "" (
+        set set conda_env=%1
+        shift
+        if "%1" NEQ "" set python_version=%1
     )
-) else (
-    pip install .
-    if errorlevel 1 (
-        echo ERROR: failed to collect dependencies with pip
-        goto END
-    )
-)
-
-call create-jk %mapstp%
-if errorlevel 1 (
     goto END
-)
 
-echo.
-echo SUCCESS!
-echo --------
-echo.
-echo Usage:
-echo.
-mapstp --help
-echo.
-echo Conda environment %mapstp% is all clear.
-echo Set your IDE to use %CONDA_PREFIX%\python.exe
-echo.
-echo Enjoy!
-echo.
-
+:reset_conda
+    echo Installing environment %conda_env% for Python %python_version%
+    call conda deactivate
+    call conda activate
+    call conda env remove -n %conda_env% -q -y
+    call conda create -n %conda_env% python=%python_version% pip -c conda-forge -q -y
+    if "%ERRORLEVEL%" NEQ "0" (
+        echo Probably conda has problems with antivirus,  try delete AppData/Temp pip files.
+    ) else (
+        call conda activate %conda_env%
+        python --version
+    )
+    goto END
 
 :END
