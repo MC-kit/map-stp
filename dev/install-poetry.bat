3e82f590d84c06abe4208b68fc436d6f08411f82
dvp <dmitri_portnov@yahoo.com>
2022-05-27 16:18:38 +0300

    style: apply more pre-commit

--- dev/install-poetry.bat
+++ dev/install-poetry.bat
@@ -2,10 +2,10 @@
 echo Installing Poetry using python script.
 echo .
 if "%POETRY_HOME%" == "" (
-	@echo standard poetry location is %USERPROFIL%\.poetry
-	@echo set environment variable POETRY_HOME in advance, if you need non-standard or shared poetry installation.
+    @echo standard poetry location is %USERPROFIL%\.poetry
+    @echo set environment variable POETRY_HOME in advance, if you need non-standard or shared poetry installation.
 ) else (
-	@echo Poetry location is %POETRY_HOME%
+    @echo Poetry location is %POETRY_HOME%
 )
 echo .
 echo The script has been downloaded from https://install.python-poetry.org at 2021-11-22, update it if necessary.
--- noxfile.py
+++ noxfile.py
@@ -14,7 +14,6 @@ from textwrap import dedent
 
 import nox
 
-# from nox.sessions import Session
 try:
     from nox_poetry import Session, session  # mypy: ignore
 except ImportError:
@@ -26,19 +25,16 @@ except ImportError:
     {sys.executable} -m pip install nox-poetry"""
     raise SystemExit(dedent(message)) from None
 
-# TODO dvp: uncomment when code and docs are more mature
 nox.options.sessions = (
     "pre-commit",
     "safety",
-    "isort",
-    "black",
+    # "isort",
+    # "black",
     "lint",
     "mypy",
     "xdoctest",
     "tests",
     "docs-build",
-    # "codecov",
-    # "docs",
 )
 
 package = "mapstp"
@@ -62,8 +58,6 @@ def activate_virtualenv_in_precommit_hooks(s: Session) -> None:
     Args:
         s: The Session object.
     """
-    assert s.bin is not None  # noqa: S101
-
     virtualenv = s.env.get("VIRTUAL_ENV")
     if virtualenv is None:
         return
@@ -223,7 +217,7 @@ def lint(s: Session) -> None:
     s.run("flake8", *args)
 
 
-@session(python=supported_pythons)
+@session(python=mypy_pythons)
 def mypy(s: Session) -> None:
     """Type-check using mypy."""
     args = s.posargs or ["src", "tests", "docs/source/conf.py"]
