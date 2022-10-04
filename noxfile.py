a93c2ca4465ed679bae3d1b6ddd4911b77514a7a
dvp <d.portnov@iterrf.ru>
2022-10-04 13:02:58 +0300

    build: stop using nox-poetry
    poetry 1.2.0 provides groups of dependencies
    this makes nox-poetry reduntant
    besides, nox-poetry is in build fail state
    for rather long time
    and it depends on poetry-export plugin
    which is now in separate project from poetry.
    Too fragile dependencies and not needed now.

--- noxfile.py
+++ noxfile.py
@@ -2,8 +2,9 @@
 
 See `Cjolowicz's article <https://cjolowicz.github.io/posts/hypermodern-python-03-linting>`_
 """
-from typing import List
+from typing import Final, List
 
+import re
 import shutil
 import sys
 
@@ -13,55 +14,54 @@ from textwrap import dedent
 
 import nox
 
-try:
-    from nox_poetry import Session, session  # mypy: ignore
-except ImportError:
-    message = f"""\
-    Nox failed to import the 'nox-poetry' package.
-
-    Please install it using the following command:
-
-    {sys.executable} -m pip install nox-poetry"""
-    raise SystemExit(dedent(message)) from None
+from nox import Session, session  # mypy: ignore
 
 nox.options.sessions = (
+    # "safety",   # TODO dvp: check if 'safety' session is necessary, if yes, return it
     "pre-commit",
-    "safety",
-    # "isort",
-    # "black",
-    # "lint",
     "mypy",
     "xdoctest",
     "tests",
     "docs-build",
 )
 
-package = "mapstp"
-locations = f"src/{package}", "src/tests", "noxfile.py", "docs/source/conf.py"
-supported_pythons = "3.8", "3.9", "3.10"
-black_pythons = "3.10"
-mypy_pythons = "3.10"
-lint_pythons = "3.10"
 
-FLAKE8_DEPS = [
-    "flake8",
-    "flake8-annotations",
-    "flake8-bandit",
-    "flake8-bugbear",
-    "flake8-builtins",
-    "flake8-colors",
-    "flake8-commas",
-    "flake8-comprehensions",
-    "flake8-docstrings",
-    "flake8-import-order",
-    "flake8-print",
-    "flake8-rst-docstrings",
-    "flake8-use-fstring",
-    "mccabe",
-    "pep8-naming",
-    "pydocstyle",
-    "tryceratops",
-]
+NAME_RGX = re.compile(r'name\s*=\s*"(?P<package>[-_a-zA-Z]+)"')
+
+
+def find_my_name() -> str:
+    """Find this package name.
+
+    Search the first row in pyproject.toml in format
+
+        name = "<package>"
+
+    and returns the <package> value.
+    This allows to reuse the noxfile.py in similar projects
+    without any changes.
+
+    Returns:
+        str: Current package found in pyproject.toml
+
+    Raises:
+        ValueError: if the pattern is not found.
+    """
+    with Path("pyproject.toml").open() as fid:
+        for line in fid:
+            res = NAME_RGX.match(line)
+            if res is not None:
+                return res["package"].replace("-", "_")
+    msg = "Cannot find package name"
+    raise ValueError(msg)
+
+
+package: Final = find_my_name()
+locations: Final = f"src/{package}", "src/tests", "noxfile.py", "docs/source/conf.py"
+
+supported_pythons: Final = "3.8", "3.9", "3.10", "3.11"
+black_pythons: Final = "3.10"
+mypy_pythons: Final = "3.10"
+lint_pythons: Final = "3.10"
 
 
 def activate_virtualenv_in_precommit_hooks(s: Session) -> None:
@@ -118,27 +118,24 @@ def activate_virtualenv_in_precommit_hooks(s: Session) -> None:
 def precommit(s: Session) -> None:
     """Lint using pre-commit."""
     args = s.posargs or ["run", "--all-files", "--show-diff-on-failure"]
-    s.install(
-        "black",
-        "darglint",
-        "pre-commit",
-        "pre-commit-hooks",
-        "isort",
-        "mypy",
-        "types-setuptools",
-        *FLAKE8_DEPS,
+    s.run(
+        "poetry",
+        "install",
+        "--only",
+        "pre_commit,style,isort,black,flake8",
+        external=True,
     )
     s.run("pre-commit", *args)
     if args and args[0] == "install":
         activate_virtualenv_in_precommit_hooks(s)
 
 
-@session(python="3.10")
-def safety(s: Session) -> None:
-    """Scan dependencies for insecure packages."""
-    requirements = s.poetry.export_requirements()
-    s.install("safety")
-    s.run("safety", "check", "--full-report", f"--file={requirements}", *s.posargs)
+# @session(python="3.10")
+# def safety(s: Session) -> None:
+#     """Scan dependencies for insecure packages."""
+#     requirements = s.poetry.export_requirements()
+#     s.install("safety")
+#     s.run("safety", "check", "--full-report", f"--file={requirements}", *s.posargs)
 
 
 @session(python=supported_pythons)
@@ -147,10 +144,10 @@ def tests(s: Session) -> None:
     s.run(
         "poetry",
         "install",
-        "--no-dev",
+        "--only",
+        "main,test,xdoctest,coverage",
         external=True,
     )
-    s.install("pytest", "pygments", "coverage[toml]")
     try:
         s.run("coverage", "run", "--parallel", "-m", "pytest", *s.posargs)
     finally:
@@ -167,7 +164,13 @@ def coverage(s: Session) -> None:
     """
     args = s.posargs or ["report"]
 
-    s.install("coverage[toml]")
+    s.run(
+        "poetry",
+        "install",
+        "--only",
+        "coverage",
+        external=True,
+    )
 
     if not s.posargs and any(Path().glob(".coverage.*")):
         s.run("coverage", "combine")
@@ -175,23 +178,23 @@ def coverage(s: Session) -> None:
     s.run("coverage", *args)
 
 
-@session(python=supported_pythons)
+# TODO dvp: check some strange errors on 3.8, 3.9 and slow install of pandas on 3.11
+@session(python="3.10")
 def typeguard(s: Session) -> None:
     """Runtime type checking using Typeguard."""
     s.run(
         "poetry",
         "install",
-        "--no-dev",
+        "--only",
+        "main,test,typeguard",
         external=True,
     )
-    s.install("pytest", "typeguard", "pygments")
     s.run("pytest", f"--typeguard-packages={package}", *s.posargs)
 
 
 @session(python="3.10")
 def isort(s: Session) -> None:
     """Organize imports."""
-    s.install("isort")
     search_patterns = [
         "*.py",
         f"src/{package}/*.py",
@@ -202,20 +205,41 @@ def isort(s: Session) -> None:
     files_to_process: List[str] = sum(
         (glob(p, recursive=True) for p in search_patterns), []
     )
-    s.run(
-        "isort",
-        "--check",
-        "--diff",
-        *files_to_process,
-        external=True,
-    )
+    if files_to_process:
+        s.run(
+            "poetry",
+            "install",
+            "--only",
+            "isort",
+            external=True,
+        )
+        s.run(
+            "pycln",
+            "--check",
+            "--diff",
+            *files_to_process,
+            external=True,
+        )
+        s.run(
+            "isort",
+            "--check",
+            "--diff",
+            *files_to_process,
+            external=True,
+        )
 
 
 @session(python=black_pythons)
 def black(s: Session) -> None:
     """Run black code formatter."""
     args = s.posargs or locations
-    s.install("black")
+    s.run(
+        "poetry",
+        "install",
+        "--only",
+        "black",
+        external=True,
+    )
     s.run("black", *args)
 
 
@@ -223,7 +247,13 @@ def black(s: Session) -> None:
 def lint(s: Session) -> None:
     """Lint using flake8."""
     args = s.posargs or locations
-    s.install(*FLAKE8_DEPS)
+    s.run(
+        "poetry",
+        "install",
+        "--only",
+        "flake8",
+        external=True,
+    )
     s.run("flake8", *args)
 
 
@@ -234,51 +264,40 @@ def mypy(s: Session) -> None:
     s.run(
         "poetry",
         "install",
-        "--no-dev",
+        "--only",
+        "main,mypy",
         external=True,
     )
-    s.install("mypy", "pytest", "types-setuptools")
     s.run("mypy", *args)
     if not s.posargs:
         s.run("mypy", f"--python-executable={sys.executable}", "noxfile.py")
 
 
-@session(python=supported_pythons)
+@session(python="3.10")
 def xdoctest(s: Session) -> None:
     """Run examples with xdoctest."""
-    args = s.posargs or ["all"]
+    args = s.posargs or ["--quiet", "-m", package]
     s.run(
         "poetry",
         "install",
-        "--no-dev",
+        "--only",
+        "main,xdoctest",
         external=True,
     )
-    s.install("xdoctest[colors]")
-    s.run("python", "-m", "xdoctest", package, *args)
+    s.run("python", "-m", "xdoctest", *args)
 
 
-@session(name="docs-build", python="3.9")
+@session(name="docs-build", python="3.10")
 def docs_build(s: Session) -> None:
     """Build the documentation."""
     args = s.posargs or ["docs/source", "docs/_build"]
     s.run(
         "poetry",
         "install",
-        "--no-dev",
+        "--only",
+        "main,docs",
         external=True,
     )
-    s.install(
-        "sphinx",
-        "sphinx-click",
-        "sphinx-rtd-theme",
-        # "sphinxcontrib-htmlhelp",
-        # "sphinxcontrib-jsmath",
-        "sphinxcontrib-napoleon",
-        # "sphinxcontrib-qthelp",
-        "sphinx-autodoc-typehints",
-        # "sphinx_autorun",
-    )
-
     build_dir = Path("docs", "_build")
     if build_dir.exists():
         shutil.rmtree(build_dir)
@@ -293,22 +312,10 @@ def docs(s: Session) -> None:
     s.run(
         "poetry",
         "install",
-        "--no-dev",
+        "--only",
+        "main,docs,docs_auto",
         external=True,
     )
-    s.install(
-        "sphinx",
-        "sphinx-autobuild",
-        "sphinx-click",
-        "sphinx-rtd-theme",
-        # "sphinxcontrib-htmlhelp",
-        # "sphinxcontrib-jsmath",
-        # "sphinxcontrib-napoleon",
-        # "sphinxcontrib-qthelp",
-        # "sphinx-autodoc-typehints",
-        # "sphinx_autorun",
-    )
-
     build_dir = Path("docs", "_build")
     if build_dir.exists():
         shutil.rmtree(build_dir)
