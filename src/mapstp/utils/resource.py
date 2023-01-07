35cd619d2eb8464297e3c2580ef0a8c1970923d1
dvp <d.portnov@iterrf.ru>
2023-01-07 11:35:47 +0300

    refact: simplify code to satisfy extended flake8 requirements

--- src/mapstp/utils/resource.py
+++ src/mapstp/utils/resource.py
@@ -19,7 +19,6 @@ def filename_resolver(package: str) -> Callable[[str], str]:
     Returns:
         callable which appends the argument to the package folder.
     """
-    # package = _resolve_package(package)
     resource_manager = pkg.ResourceManager()  # type: ignore[attr-defined]
 
     def func(resource: str) -> str:
@@ -33,7 +32,7 @@ def filename_resolver(package: str) -> Callable[[str], str]:
 def path_resolver(package: str) -> Callable[[str], Path]:
     """Create method to find data path.
 
-    Uses :py:func:`file_resolver`.
+    Uses :func:`file_resolver`.
 
     Args:
         package: the package below which the data is stored.
@@ -42,8 +41,6 @@ def path_resolver(package: str) -> Callable[[str], Path]:
     Returns:
         callable which appends the argument to the package folder adt returns as Path.
     """
-    # Note: we should define package here to have proper offset in callers stack.
-    # package = _resolve_package(package)
     resolver = filename_resolver(package)
 
     def func(resource: str) -> Path:
@@ -53,26 +50,3 @@ def path_resolver(package: str) -> Callable[[str], Path]:
     func.__doc__ = f"Computes Path for resources located in {package}"
 
     return func
-
-
-# class PackageNotFoundError(ValueError):
-#     """Error when package is not specified and cannot be found."""
-#
-#     def __init__(self) -> None:
-#         super().__init__("Cannot define package.")  # pragma: no cover
-#
-#
-# def _resolve_package(
-#     package,
-# ):
-#     # No typing in this function,
-#     # otherwise typeguard decorates this and
-#     # the frame offset is to be found in a bit
-#     # more complex way.
-#     # ANN202 is ignored for this file (see .flake8)
-#     if package is None:
-#         module = inspect.getmodule(inspect.stack()[2][0])
-#         if module is None:
-#             raise PackageNotFoundError()  # pragma: no cover
-#         package = module.__name__
-#     return package
