a0a2ed597d203c6f9800062a8bceffd3f4ff8f2c
dvp <d.portnov@iterrf.ru>
2023-03-07 17:19:41 +0300

    build: stop using deprecated pkg_resources

--- src/mapstp/utils/resource.py
+++ src/mapstp/utils/resource.py
@@ -1,37 +1,21 @@
 """Utility methods to access a package data."""
+from __future__ import annotations
 
-from typing import Callable, cast
+from typing import Callable
+
+import sys
 
 from pathlib import Path
 
-import pkg_resources as pkg
+if sys.version_info >= (3, 9):
+    from importlib.abc import Traversable
+    from importlib.resources import Package, files
+else:
+    from importlib_resources import files
 
 
-def filename_resolver(package: str) -> Callable[[str], str]:
-    """Create method to find data file name.
-
-    Uses resource manager to handle all the cases of the deployment.
-
-    Args:
-        package: the package below which the data is stored.
-
-    Returns:
-        callable which appends the argument to the package folder.
-    """
-    resource_manager = pkg.ResourceManager()  # type: ignore[attr-defined]
-
-    def func(resource: str) -> str:
-        return cast(str, resource_manager.resource_filename(package, resource))
-
-    func.__doc__ = f"Computes file names for resources located in {package}"
-
-    return func
-
-
-def path_resolver(package: str) -> Callable[[str], Path]:
+def path_resolver(package: Package) -> Callable[[str], Path | Traversable]:
     """Create method to find data path.
-
-    Uses :func:`file_resolver`.
 
     Args:
         package: the package below which the data is stored.
@@ -39,12 +23,4 @@ def path_resolver(package: str) -> Callable[[str], Path]:
     Returns:
         callable which appends the argument to the package folder adt returns as Path.
     """
-    resolver = filename_resolver(package)
-
-    def func(resource: str) -> Path:
-        filename = resolver(resource)
-        return Path(filename)
-
-    func.__doc__ = f"Computes Path for resources located in {package}"
-
-    return func
+    return files(package).joinpath
