c88e7e0a2708b91a8ad476a20aafe5f16a1116c0
dvp <d.portnov@iterrf.ru>
2023-10-30 15:20:37 +0300

    refact: remove overcomplication

--- src/mapstp/cli/mapstp_logging.py
+++ src/mapstp/cli/mapstp_logging.py
@@ -7,12 +7,12 @@ from __future__ import annotations
 from typing import Final
 
 import logging
+import os
 import sys
 
 from pathlib import Path
 
 from loguru import logger
-from mapstp.config import env
 
 
 class InterceptHandler(logging.Handler):
@@ -45,16 +45,17 @@ log.addHandler(InterceptHandler())
 
 # from loguru._defaults.py
 
-MAPSTP_CONSOLE_LOG_FORMAT: Final[str] = env(
+MAPSTP_CONSOLE_LOG_FORMAT: Final[str] = os.getenv(
     "MAPSTP_CONSOLE_LOG_FORMAT",
-    default="<green>{time:YYYY-MM-DD HH:mm:ss}</green> | "
+    "<green>{time:YYYY-MM-DD HH:mm:ss}</green> | "
     "<level>{level: <8}</level> | "
     "<level>{message}</level>",
 )
-MAPSTP_FILE_LOG_PATH: Final[Path] = env(
-    "MAPSTP_FILE_LOG_PATH",
-    converter=Path,
-    default="mapstp.log",
+MAPSTP_FILE_LOG_PATH: Final[Path] = Path(
+    os.getenv(
+        "MAPSTP_FILE_LOG_PATH",
+        "mapstp.log",
+    ),
 )
 
 
